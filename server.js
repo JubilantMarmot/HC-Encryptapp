aebc02e8a21bd65eb64b8a5ea1c8dba096102702
JubilantMarmot <abboudalomari6@gmail.com>
2024-08-18 07:16:22 +0000

    Update server.js

--- server.js
+++ server.js
@@ -4,35 +4,60 @@ const port = 3000;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('test');
-});
-
 app.post('/encode', (req, res) => {
-    const { data } = req.body;
+    const { data, cipher } = req.body;
     if (typeof data !== 'string') {
         return res.status(400).json({ error: 'Data must be a string' });
     }
-    const encodedData = Buffer.from(data).toString('base64');
-    res.json({ encodedData });
+    if (cipher === 'base64') {
+        const encodedData = Buffer.from(data).toString('base64');
+        res.json({ encodedData });
+    } else if (cipher === 'caesar') {
+        const shift = 3; // Shift by 3 for Caesar Cipher
+        const encodedData = caesarCipher(data, shift);
+        res.json({ encodedData });
+    } else {
+        res.status(400).json({ error: 'Unsupported cipher type' });
+    }
 });
 
 app.post('/decode', (req, res) => {
-    const { encodedData } = req.body;
+    const { encodedData, cipher } = req.body;
     if (typeof encodedData !== 'string') {
         return res.status(400).json({ error: 'Encoded data must be a string' });
     }
-    try {
-        const decodedData = Buffer.from(encodedData, 'base64').toString('utf8');
+    if (cipher === 'base64') {
+        try {
+            const decodedData = Buffer.from(encodedData, 'base64').toString('utf8');
+            res.json({ decodedData });
+        } catch (error) {
+            res.status(400).json({ error: 'Invalid BASE64 encoded data' });
+        }
+    } else if (cipher === 'caesar') {
+        const shift = 3; // i looked it up and it seems that 3 is alwayus used
+        const decodedData = caesarCipher(encodedData, -shift);
         res.json({ decodedData });
-    } catch (error) {
-        res.status(400).json({ error: 'Invalid BASE64 encoded data' });
+    } else {
+        res.status(400).json({ error: 'Unsupported cipher type' });
     }
 });
 
+function caesarCipher(text, shift) {
+    return text.split('')
+        .map(char => {
+            if (char.match(/[a-z]/i)) {
+                const code = char.charCodeAt();
+                const base = char >= 'a' && char <= 'z' ? 97 : 65;
+                return String.fromCharCode(((code - base + shift + 26) % 26) + base);
+            }
+            return char;
+        })
+        .join('');
+}
+
 app.use((err, req, res, next) => {
     console.error(err.stack);
-    res.status(500).send('Something broke!');
+    res.status(500).send('Something went wrong please try again later. Thank you');
 });
 
 app.listen(port, () => {
